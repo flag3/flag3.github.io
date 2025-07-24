7cd183d003448b093ea05c74421f8fffc8bf9670
flag3 <flag3833753@gmail.com>
2025-07-25 02:00:13 +0900

    remove: Vercel Analytics and Speed Insights dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar } from "./components/nav";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./components/footer";
 import { ThemeProvider } from "./components/theme-switch";
 import { metaData } from "./lib/config";
@@ -84,8 +82,6 @@ export default function RootLayout({
             <Navbar />
             {children}
             <Footer />
-            <Analytics />
-            <SpeedInsights />
           </main>
         </ThemeProvider>
       </body>
